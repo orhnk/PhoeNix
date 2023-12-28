45fa42327cf92d143d21d0b495e5a4601fb26bb8
orhnk <utfeightt@gmail.com>
2023-12-28 22:59:29 +0300

    fix: remove newlines

--- templates/dmenuTheme.nix
+++ templates/dmenuTheme.nix
@@ -1,3 +1,3 @@
-theme: with theme.withHashtag; ''
--b -nb '${base00}' -nf '${base07}' -sb '${base0C}' -sf '${base00}'
-''
+theme: with theme.withHashtag; 
+" -b -nb '${base00}' -nf '${base07}' -sb '${base0C}' -sf '${base00}' "
+# wrapped with spaces: `"dmenu" + ${dmenuTheme}` could cause: `dmenu-b -nb ...`
